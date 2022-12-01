4c97b9727abcdf0e579e27939bc134d1b10a6c57
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2022-12-01 22:32:32 +0100

    add possibility to skip solvers + generate graphs

--- benchmark.sh
+++ benchmark.sh
@@ -43,17 +43,68 @@ else
     echo "schlandals binary found"
 fi
 
-if [ $# -ne 1 ]
+if [ $# -lt 2 ]
 then
-    printf "You must pass 1 argument, the number of thread that will be used by parallel\n"
+    printf "You must pass at least 2 arguments: i) the number of threads used by parallel ii) the name of the solvers to benchmarks\n"
+    printf "If a solver is not benchmarked, its last benchmarks will be copied"
     exit 1
 fi
+run_ganak=false
+run_projMC=false
+run_schlandals=false
+
+shift
+while [ $# -ne 0 ]
+do
+    if [ $1 == "ganak" ]
+    then
+        run_ganak=true
+    fi
+    if [ $1 == "projMC" ]
+    then
+        run_projMC=true
+    fi
+    if [ $1 == "schlandals" ]
+    then
+        run_schlandals=true
+    fi
+    shift
+done
+
+last_bench_dir=$(ls results/ | sort -i -r | head -n 1)
+
 nb_thread=$1
 nb_repeat=5
 
 timestamp=$(date +%Y_%m_%d_%H_%M_%s)
 output_dir=results/$timestamp
 mkdir -p $output_dir
+
+# copying the old benchmarks if any, for the solver not benchmarked
+# if no benchmarks present, exit
+echo $run_ganak
+echo $run_projMC
+echo $run_schlandals
+echo $last_bench_dir
+if [ $run_ganak = false ]
+then
+    cp -r results/$last_bench_dir/ganak $output_dir/ganak
+else
+    mkdir $output_dir/ganak
+fi
+if [ $run_projMC = false ]
+then
+    cp -r results/$last_bench_dir/projMC $output_dir/projMC
+else
+    mkdir $output_dir/projMC
+fi
+if [ $run_schlandals = false ]
+then
+    cp -r results/$last_bench_dir/schlandals $output_dir/schlandals
+else
+    mkdir $output_dir/schlandals
+fi
+
 # Time format for the time commmand. Real_User_Kernel
 export TIMEFORMAT=%R_%U_%S
 export par_cmd="parallel --bar -j $nb_thread"
@@ -61,9 +112,6 @@ timeout=120
 export ganak_cmd="$timeout_cmd ganak -q"
 export projMC_cmd="$timeout_cmd d4 -m projMC -i"
 export schlandals_cmd="$timeout_cmd schlandals --branching articulation -i"
-mkdir $output_dir/ganak
-mkdir $output_dir/projMC
-mkdir $output_dir/schlandals
 
 # First set of benchmarks, bayesian networks
 printf "Benchmarking bayesian networks\n"
@@ -86,6 +134,8 @@ $par_cmd --results $output_dir/ganak/pg.csv "time (bash -c 'ulimit -t $timeout; 
 $par_cmd --results $output_dir/projMC/pg.csv "time (bash -c 'ulimit -t $timeout; $projMC_cmd {2} >> /dev/null')" ::: $(seq $nb_repeat) ::: $(find instances/power_transmission_grid/ -type f -name '*.cnf')
 printf "\tLaunching ppidimacs files (schlandals)\n"
 $par_cmd --results $output_dir/schlandals/pg.csv "time (bash -c 'ulimit -t $timeout; $schlandals_cmd {2} >> /dev/null')" ::: $(seq $nb_repeat) ::: $(find instances/power_transmission_grid/ -type f -name '*.ppidimacs')
+
+python3 graphs.py $timestamp ganak projMC schlandals
 
 git add results/$timestamp
 git commit -m "auto commit results $timestamp"
