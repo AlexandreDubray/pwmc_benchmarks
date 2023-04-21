354f2a1252797b7e351d13e956646d74f34cd9c0
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2023-04-21 15:42:51 +0200

    compress CPTs for ppidimacs files

--- instances/bayesian_networks/instances_from_uai.py
+++ instances/bayesian_networks/instances_from_uai.py
@@ -4,7 +4,16 @@ import re
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
+def remove_trailing_space(l):
+    for i in range(len(l)):
+        l[i] = l[i].replace('  ', ' ')
+
 def parse_file(dataset):
+    evidence = {}
+    with open(os.path.join(script_dir, 'uai', dataset + '.evid')) as f:
+        for line in f:
+            s = line.rstrip().split(' ')
+            evidence[int(s[0])] = int(s[1])
     with open(os.path.join(script_dir, 'uai', dataset + '.uai')) as f:
         all_file = f.read().replace('  ', ' ')
         lines = all_file.split('\n')[1:]
@@ -20,7 +29,6 @@ def parse_file(dataset):
         ncpt = int(lines[idx])
         idx += 1
         cpts_def = []
-        deterministic_index = 0
         is_leaf = [True for _ in range(nvar)]
         for i in range(ncpt):
             vs = lines[idx].rstrip().split(' ')[1:]
@@ -28,16 +36,52 @@ def parse_file(dataset):
                 is_leaf[int(x)] = False
             idx += 1
             cpts_def.append([int(x) for x in vs])
-            offset = 1
-            for v in cpts_def[-1]:
-                offset *= len(variables[v])
-            deterministic_index += offset
-        for v in range(nvar):
+
+        idx += 1 # blank line
+
+        probabilistic_index = 0
+        map_weight = {}
+        proba_vars = [[] for v in range(len(variables))]
+        cpt_idx = 0
+        for i in range(idx+1, len(lines), 2):
+            probas = [float(x) for x in lines[i].split(' ')]
+            distributions = []
+            v = cpts_def[cpt_idx][-1]
+            cpt_idx += 1
+            dom_size = len(variables[v])
+            ds = []
+            sorted_ds = []
+            for nd in range(int(len(probas) / dom_size)):
+                ds.append([x for x in probas[nd*dom_size:(nd+1)*dom_size]])
+                sorted_ds.append(sorted(ds[-1]))
+            nb_proba_index_needed = 0
+            for i in range(len(ds)):
+                d = ds[i]
+                found_idx = None
+                for j in range(max(0, i-1), i):
+                    if set(ds[i]) == set(ds[j]):
+                        found_idx = j
+                        break
+                if found_idx is not None:
+                    m = {}
+                    for p in d:
+                        m[p] = proba_vars[v][found_idx][p]
+                    proba_vars[v].append(m)
+                else:
+                    m = {}
+                    for p in d:
+                        if p not in m:
+                            m[p] = probabilistic_index
+                            map_weight[probabilistic_index] = p
+                            probabilistic_index += 1
+                    proba_vars[v].append(m)
+        
+        deterministic_index = probabilistic_index
+        for v in range(len(variables)):
             for i in range(len(variables[v])):
                 variables[v][i] = deterministic_index
                 deterministic_index += 1
         
-        idx += 1 # blank line
         cpt = []
         distributions_m = {}
         for v in range(nvar):
@@ -46,15 +90,16 @@ def parse_file(dataset):
             body_vars = list(itertools.product(*ttt))
             idx += 1
             probas = [float(x) for x in lines[idx].rstrip().split(' ')]
+                       
             idx += 1
             c = []
             for i in range(len(probas)):
                 tt = [x for x in body_vars[i]]
                 p = probas[i]
                 try:
-                    distributions_m[tuple([v] + tt[:-1])].append((tt[-1], p))
+                    distributions_m[tuple([v] + tt[:-1])].append((tt[-1], proba_vars[v][int(i / dom_size)][p], p))
                 except KeyError:
-                    distributions_m[tuple([v] + tt[:-1])] = [(tt[-1], p)]
+                    distributions_m[tuple([v] + tt[:-1])] = [(tt[-1], proba_vars[v][int(i / dom_size)][p], p)]
             cpt.append(c)
 
         # Handling enc4 format
@@ -84,16 +129,27 @@ def parse_file(dataset):
         clauses = []
         clauses_cnf = []
         cnf_weights = []
-        probabilistic_index = 0
+        d_weights = []
         
+        for evid in evidence:
+            for i in range(len(variables[evid])):
+                if i != evidence[evid]:
+                    clauses_cnf.append(f'-{variables[evid][i] + 1} 0')
+        
+        for vidx in map_weight:
+            #d_weights.append(f'c p weight {vidx} {map_weight[vidx]:.9f}')
+            cnf_weights.append(f'c p weight {vidx + 1} {map_weight[vidx]:.9f} 0')
+            cnf_weights.append(f'c p weight -{vidx + 1} {(1 - map_weight[vidx]):.9f} 0')
+        
+        #distributions.append('c p show ' + ' '.join([str(x) for x in range(probabilistic_index)]))
+        seen_distrib = set()
         for sub_body in distributions_m:
-            distributions.append(f'd {" ".join([f"{y:.5f}" for x,y in distributions_m[sub_body]])}')
-            for end_body, proba in distributions_m[sub_body]:
-                clauses.append(f'{end_body} {" ".join([str(-x) for x in sub_body[1:]])} -{probabilistic_index}')
-                clauses_cnf.append(f'{end_body + 1} {" ".join([str(-(x+1)) for x in sub_body[1:]])} -{probabilistic_index+1} 0')
-                cnf_weights.append(f'c p weight {probabilistic_index + 1} {proba:.5f} 0')
-                cnf_weights.append(f'c p weight -{probabilistic_index + 1} {(1-proba):.5f} 0')
-                probabilistic_index += 1
+            distrib_idx = tuple([y for x,y,z in distributions_m[sub_body]])
+            if tuple not in seen_distrib:
+                distributions.append('d ' + ' '.join(["{:.9f}".format(map_weight[x]) for x in distrib_idx]))
+            for end_body, proba_var_idx, proba in distributions_m[sub_body]:
+                clauses.append(f'{end_body} {" ".join([str(-x) for x in sub_body[1:]])} -{proba_var_idx}')
+                clauses_cnf.append(f'{end_body + 1} {" ".join([str(-(x+1)) for x in sub_body[1:]])} -{proba_var_idx} 0')
         
         for vs in variables:
             clauses_cnf.append(f'{" ".join([str(x+1) for x in vs])} 0')
@@ -105,14 +161,21 @@ def parse_file(dataset):
         os.makedirs(os.path.join(script_dir, 'pcnf', dataset), exist_ok=True)
         os.makedirs(os.path.join(script_dir, 'enc4', dataset), exist_ok=True)
         file_idx = 0
+        remove_trailing_space(distributions)
+        remove_trailing_space(clauses)
+        remove_trailing_space(clauses_cnf)
+        remove_trailing_space(cnf_weights)
+        remove_trailing_space(d_weights)
+        remove_trailing_space(enc4_weights)
+        remove_trailing_space(enc4_clauses)
         for i in range(nvar):
             if is_leaf[i]:
                 dom_size = len(variables[i])
                 for j in range(len(variables[i])):
-                    """
                     with open(os.path.join(script_dir, 'ppidimacs', dataset, f'{file_idx}.ppidimacs'), 'w') as fout:
                         fout.write(f'p cnf {deterministic_index} {len(clauses) + dom_size - 1}\n')
                         fout.write('\n'.join(distributions) + '\n')
+                        #fout.write('\n'.join(d_weights) + '\n')
                         fout.write('\n'.join(clauses) + '\n')
                         for k in range(len(variables[i])):
                             if k != j:
@@ -156,4 +219,5 @@ instances = [
 ]
 
 for instance in instances:
+    print(instance)
     parse_file(instance)
