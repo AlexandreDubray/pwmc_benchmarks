d1eb7e815435f7e1d41d45ae5d00747ceec13ebf
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2023-05-04 14:29:49 +0200

    use implicit for enc4linp encoding

--- instances/bayesian_networks/uai_to_cnf.sh
+++ instances/bayesian_networks/uai_to_cnf.sh
@@ -6,5 +6,5 @@ for inst in "${instances[@]}"
 do
     ./bn2cnf_linux -i uai/$inst.uai -o enc3_tpl/$inst.cnf -w enc3_tpl/$inst.weights -v enc3_tpl/$inst.map
     ./bn2cnf_linux -i uai/$inst.uai -o enc4_tpl/$inst.cnf -w enc4_tpl/$inst.weights -v enc4_tpl/$inst.map -s prime
-    ./bn2cnf_linux -i uai/$inst.uai -o enc4_log_tpl/$inst.cnf -w enc4_log_tpl/$inst.weights -v enc4_log_tpl/$inst.map -s prime -e LOG
+    ./bn2cnf_linux -i uai/$inst.uai -o enc4_log_tpl/$inst.cnf -w enc4_log_tpl/$inst.weights -v enc4_log_tpl/$inst.map -s prime -e LOG -implicit
 done
