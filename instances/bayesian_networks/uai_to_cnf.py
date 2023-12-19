eafbc25db7acaede75aeadc6d999fb0f6e0020ff
Lucile Dierckx <lucile.dierckx@uclouvain.be>
2023-12-19 10:01:58 +0100

    pysdd encoding

--- instances/bayesian_networks/uai_to_cnf.py
+++ instances/bayesian_networks/uai_to_cnf.py
@@ -78,6 +78,32 @@ def make_enc_encoding(dataset, outdir):
                 file_idx += 1
 
 
+def make_pysdd_encoding(dataset, outdir):
+    os.makedirs(os.path.join(outdir, dataset), exist_ok=True)
+    output = parse_exe_output()
+    network_data = get_network_data(dataset)
+    file_idx = 1
+    for network_var in range(len(network_data['dom_size'])):
+        if network_data['leaf'][network_var]:
+            dom_size = network_data['dom_size'][network_var]
+            for variable_value in range(dom_size):
+                n_clause = output['nclauses'] + len(output['variable_map'][network_var])
+                print(os.path.join(outdir, dataset, f'{file_idx}.cnf'))
+                with open(os.path.join(outdir, dataset, f'{file_idx}.cnf'), 'w') as f:
+                    weights = ["0.0"]*output['nvars']*2
+                    for v in output['weights']:
+                        if v > 0:
+                            weights[(v-1)*2] = str(output['weights'][v])
+                        else:
+                            weights[(-v-1)*2+1] = str(output['weights'][v])
+                    f.write(f'c weights {" ".join(weights)}\n')
+                    f.write(f'p cnf {output["nvars"]} {n_clause-1}\n')
+                    f.write(''.join(output["clauses"]))
+                    
+                    for value_indicator_variables in output['variable_map'][network_var][variable_value]:
+                        f.write(f'{value_indicator_variables} 0\n')
+                file_idx += 1
+
 def enc3(dataset):
     bash_cmd = ['./bn2cnf_linux', '-i', f'uai/{dataset}.uai', '-o', 'tmp.cnf', '-w', 'tmp.weight', '-v', 'tmp.map']
     subprocess.run(bash_cmd, stdout=subprocess.DEVNULL)
@@ -93,14 +119,21 @@ def enc4linp(dataset):
     subprocess.run(bash_cmd, stdout=subprocess.DEVNULL)
     make_enc_encoding(dataset, os.path.join(script_dir, 'enc4linp'))
 
+def pysdd(dataset):
+    #based on enc4linp
+    bash_cmd = ['./bn2cnf_linux', '-i', f'uai/{dataset}.uai', '-o', 'tmp.cnf', '-w', 'tmp.weight', '-v', 'tmp.map', '-s', 'prime', '-e', 'LOG', '-implicit']
+    subprocess.run(bash_cmd, stdout=subprocess.DEVNULL)
+    make_pysdd_encoding(dataset, os.path.join(script_dir, 'pysdd'))
+
 
 instances = [f.split('.')[0] for f in os.listdir(os.path.join(script_dir, 'uai')) if os.path.isfile(os.path.join(script_dir, 'uai', f))]
 
 for instance in instances:
     print(instance)
-    enc3(instance)
+    """ enc3(instance)
     enc4(instance)
-    enc4linp(instance)
+    enc4linp(instance) """
+    pysdd(instance) #based on enc4linp
 
 os.remove(os.path.join(script_dir, 'tmp.cnf'))
 os.remove(os.path.join(script_dir, 'tmp.weight'))
